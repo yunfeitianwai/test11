9ddea87768c48df2dd3829d18a4a06a945142e1e
yunfeitianwai <73921585+yunfeitianwai@users.noreply.github.com>
2022-10-19 14:12:44 +0800

    Update zy_jd_speed_sign_Part1.js

--- zy_jd_speed_sign_Part1.js
+++ zy_jd_speed_sign_Part1.js
@@ -50,7 +50,7 @@ const JD_API_HOST = 'https://api.m.jd.com/', actCode = 'visa-card-001';
         continue
       }
       await jdGlobal()
-      await $.wait(10*1000)
+      await $.wait(2*1000)
 	  
 	  if (llAPIError){
 		console.log(`黑IP了，赶紧重新拨号换个IP吧`);
@@ -82,8 +82,8 @@ async function jdGlobal() {
     await taskList()
     await queryJoy()
     // await signInit()
-    await cash()
-    await showMsg()
+   // await cash()
+    //await showMsg()
   } catch (e) {
     $.logErr(e)
   }
